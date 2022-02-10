f1c08a6ff786c411dd1418349e0bc074061e3523
Michael Aular <michaelaular@gmail.com>
2022-02-10 09:08:56 +0100

    updated oops page

--- src/routes/oops.js
+++ src/routes/oops.js
@@ -9,6 +9,8 @@ export default function Oops() {
         <>
         <div className="oops"><h2>OOPS!</h2>
         <p>It seems like you took a wrong turn somewhere in life...</p>
+        <p>Here have a sandal.</p>
+        <img className="oops_icon" src={require(`../images/icons/sanicon.png`)} />
         </div>
         </>
     );
