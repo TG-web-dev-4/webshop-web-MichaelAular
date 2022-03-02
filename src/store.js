ed3bb30e361ac28a6639c829b9dea9394f775bcb
Michael Aular <michaelaular@gmail.com>
2022-03-02 11:14:09 +0100

    persist store

--- src/store.js
+++ src/store.js
@@ -2,9 +2,21 @@ import { createStore } from "redux";
 import { devToolsEnhancer } from "redux-devtools-extension";
 import  rootReducer  from "./reducers/rootReducer";
 import product from "./data";
+import {persistStore, persistReducer} from "redux-persist";
+import storage from "redux-persist/lib/storage";
+
+
+const persistConfig = {
+    key: "root",
+    storage
+};
+
+const persistedReducer = persistReducer(persistConfig, rootReducer)
 
 export const store = createStore(
-    rootReducer,
+    persistedReducer,
     {productReducer:product},
     devToolsEnhancer()
     );
+
+    const persistor = persistStore(store);
